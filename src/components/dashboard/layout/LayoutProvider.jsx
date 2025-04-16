cc9079511eb770fc05b0c777ec7801a1d9c21cc9
Maverick-Services <maverickit123@gmail.com>
2025-04-17 00:00:15 +0530

    firebase setup

--- src/components/dashboard/layout/LayoutProvider.jsx
+++ src/components/dashboard/layout/LayoutProvider.jsx
@@ -13,6 +13,18 @@ function LayoutProvider({ children }) {
 
     return (
         <main className='flex flex-col sm:flex-row h-screen relative'>
+            <div className="w-full max-[768px]:flex-col flex bg-[#F3F6FE] h-screen">
+                <div className="flex w-full justify-between items-center p-5 bg-white shadow-md md:hidden">
+                    <img src="/logo.png" alt="logo" className="h-7" />
+                    <div onClick={() => setSidebarOpen(prev => !prev)} className="cursor-pointer">
+                        {sidebarOpen
+                            ? <IoClose className="text-2xl ease-in-out transition-all duration-300 focus:rotate-90" />
+                            : <FiMenu className="text-2xl" />
+                        }
+                    </div>
+                </div>
+                <Sidebar isOpen={sidebarOpen} setIsSidebarOpen={setSidebarOpen} />
+            </div>
             <div className='hidden sm:block'>
                 <Sidebar />
             </div>
---
+++ src/lib/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtfHfc1a2ySX0kh-XvDCfBnR2quqhkMD0",
+  authDomain: "apoorv-cab-booking.firebaseapp.com",
+  projectId: "apoorv-cab-booking",
+  storageBucket: "apoorv-cab-booking.firebasestorage.app",
+  messagingSenderId: "716598123326",
+  appId: "1:716598123326:web:aad235467306547c422a6f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = typeof window !== "undefined" ? getAuth(app) : null;
