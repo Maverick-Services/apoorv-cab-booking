a8d8b5dbb3e2d87d2be38b1cb216f4a9310e69df
Maverick-Services <maverickit123@gmail.com>
2025-04-22 13:28:28 +0530

    pickup city updates

--- src/app/admin/pickup-cities/page.jsx
+++ src/app/admin/pickup-cities/page.jsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import PickupCityCards from './components/PickupCityCards';
 import { getAllPickupCities } from '@/lib/firebase/admin/pickupCity'
+import { Loader2 } from 'lucide-react'
 
 function page() {
 
@@ -33,13 +34,24 @@ function page() {
             <InnerLayout heading={"Pickup Cities"}>
                 <div className='flex justify-between items-center w-full mb-6'>
                     <div className="text-lg font-medium text-muted-foreground">
-                        Total Cities: {20}
+                        Total Cities: {pickupCities.length}
                     </div>
                     <Link href={'/admin/pickup-cities/form'}>
                         <Button className="cursor-pointer">Add New</Button>
                     </Link>
                 </div>
-                <PickupCityCards />
+
+                {loading ? (
+                    <div className="col-span-full flex justify-center py-10">
+                        <Loader2 className="animate-spin w-8 h-8 text-primary" />
+                    </div>
+                ) : pickupCities.length === 0 ? (
+                    <div className="col-span-full text-center text-muted-foreground py-10">
+                        No Pickup City found.
+                    </div>
+                ) : (
+                    <PickupCityCards pickupCities={pickupCities} />
+                )}
             </InnerLayout>
         </div>
     )
