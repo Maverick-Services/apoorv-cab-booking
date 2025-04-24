7f6743904fdb4a4bf3aae827f270a42e536c4fcc
Maverick-Services <maverickit123@gmail.com>
2025-04-24 11:14:14 +0530

    login updates

--- src/components/main/bookingPage/LoginDialog.jsx
+++ src/components/main/bookingPage/LoginDialog.jsx
@@ -1,26 +1,47 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Save } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
+import Login from './Login'
+import Signup from './Signup'
 
 function LoginDialog({ open, onOpenChange, onSave }) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [name, setName] = useState("")
+    const [loading, setLoading] = useState(false)
+    const handleSignup = async (e) => {
+    }
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
             <DialogContent>
-                <div className="mx-auto w-full max-w-sm">
+                <div className="mx-auto w-full px-3 pb-3">
                     <DialogHeader>
-                        <DialogTitle>Edit Variant Details</DialogTitle>
-                        <DialogDescription>Make changes to your item variant here.</DialogDescription>
+                        {/* <DialogTitle>Join Us Now</DialogTitle> */}
                     </DialogHeader>
 
+                    <Tabs defaultValue="inclusions" className="w-full mt-4">
+                        <TabsList className="w-full grid grid-cols-2 bg-gray-100 rounded-lg p-1 mb-4">
+                            <TabsTrigger value="login">Login</TabsTrigger>
+                            <TabsTrigger value="signup">Signup</TabsTrigger>
+                        </TabsList>
 
+                        <TabsContent value="login">
+                            <Login />
+                        </TabsContent>
 
-                    <DialogFooter>
-                        <Button onClick={onSave} className="text-white flex items-center gap-2">
+                        <TabsContent value="signup">
+                            <Signup />
+                        </TabsContent>
+                    </Tabs>
+                    {/* <DialogFooter>
+                        <Button onClick={onSave} className="text-white flex items-center gap-2 mt-4">
                             <Save className="h-4 w-4" />
                             Save Changes
                         </Button>
-                    </DialogFooter>
+                    </DialogFooter> */}
                 </div>
             </DialogContent>
         </Dialog>
