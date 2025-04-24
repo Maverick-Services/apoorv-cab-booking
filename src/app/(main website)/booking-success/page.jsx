237a9ce18ef4a47c3021c8b7c69384ff1d297e79
Maverick-Services <maverickit123@gmail.com>
2025-04-24 17:01:46 +0530

    Booking Update

--- src/app/(main website)/booking-success/page.jsx
+++ src/app/(main website)/booking-success/page.jsx
@@ -3,7 +3,7 @@ import Navbar from '@/components/main/navbar/Navbar';
 import React, { Suspense } from 'react';
 import BookingData from './components/BookingData';
 
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 function page() {
     return (
@@ -12,9 +12,9 @@ function page() {
                 <Navbar />
             </div>
             <div className='text-black pb-10 pt-1 w-11/12 mx-auto flex flex-col justify-between'>
-                <Suspense fallback={<div>Loading...</div>}>
-                    <BookingData />
-                </Suspense>
+                {/* <Suspense fallback={<div>Loading...</div>}> */}
+                <BookingData />
+                {/* </Suspense> */}
             </div>
             <Footer />
         </div>
