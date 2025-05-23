4b2297b39adc8c1db715279924ce133a45078653
Maverick-Services <maverickit123@gmail.com>
2025-05-23 21:23:46 +0530

    Live Chatbot Integrated

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,7 @@ import 'slick-carousel/slick/slick-theme.css';
 import "./globals.css";
 import AuthProvider from "@/components/main/AuthProvider";
 import { Toaster } from "react-hot-toast";
+import Script from 'next/script';
 
 export const metadata = {
   title: "Taps Cabs",
@@ -24,8 +25,25 @@ export default function RootLayout({ children }) {
           {children}
           <Toaster position="top-right" reverseOrder={false} />
         </AuthProvider>
+
+        <Script
+          id="tawkto-widget"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+              (function(){
+                var s1 = document.createElement("script"),s0 = document.getElementsByTagName("script")[0];
+                s1.async = true;
+                s1.src = 'https://embed.tawk.to/5a2906d0d0795768aaf8ddfb/default';
+                s1.charset = 'UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1, s0);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
 }
-
